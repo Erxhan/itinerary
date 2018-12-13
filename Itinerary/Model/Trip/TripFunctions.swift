eebb3571e476ecf252ff462c9eca0e9009a71b72
Erxhan Selmani <ese@radionomy.com>
2018-12-13 15:18:14 +0100

    Added model and function to Trip

---
+++ Itinerary/Model/Trip/TripFunctions.swift
@@ -0,0 +1,27 @@
+//
+//  TripFunctions.swift
+//  Itinerary
+//
+//  Created by Exrhan Selmani on 13/12/2018.
+//  Copyright © 2018 Erxhan Selmani. All rights reserved.
+//
+
+import Foundation
+
+class TripFunctions {
+    static func createTrip(tripModel: TripModel) {
+        
+    }
+    
+    static func readTrips() {
+        
+    }
+    
+    static func updateTrip(tripModel: TripModel) {
+        
+    }
+    
+    static func deleteTrip(tripModel: TripModel) {
+        
+    }
+}
---
+++ Itinerary/Model/Trip/TripModel.swift
@@ -0,0 +1,19 @@
+//
+//  TripModel.swift
+//  Itinerary
+//
+//  Created by Exrhan Selmani on 13/12/2018.
+//  Copyright © 2018 Erxhan Selmani. All rights reserved.
+//
+
+import Foundation
+
+class TripModel {
+    var id: String!
+    var title: String!
+    
+    init(title: String) {
+        id = UUID().uuidString
+        self.title = title
+    }
+}
